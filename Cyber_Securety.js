92ffc4fd6b78b8f90615854ebc6378749ddb4dad
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-01 08:42:29 -0700

    Create Cyber_Securety.js

---
+++ Cyber_Securety.js
@@ -0,0 +1,13 @@
+input userbot.get
+input legendBot ,
+  await:sendhacker_(09876565657),send_none)()*
+            +legend+
+               none<ok"legendhacker"({os}),h/n"
+        none/n"""
+ none(os)"hack_input)"""
+   "alive" send_messege,get userbot.send username#leeggenddhhacker*
+     'alive:await;
+ await(os).import*
+       os = import*(legend)
+     print("legend").get
+      legendhacker = int(str(none,(java_script")(*).linux
