f42049339e5b803f6b630570b05630509a2ce978
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-07 08:46:40 -0700

    Create SATYA.py

---
+++ SATYAUSERBOT/SATYA.py
@@ -0,0 +1,16 @@
+from userbot /root/userbot.bot
+from hackers , pythonbot
+from utils.Clints
+import userbot.userbot_telegram
+  
+  
+          base64 -d ||SaTyauserbotlegendbotownersupport=||-d
+          satya=mirror_z
+           await:satya_userbot.user.telethon()
+          try():
+              legendboy=int("legendboy")str{userbot.utils}).bot
+              legend await mirror_mod==legendbot(intput=False:
+              userbot.root:
+                 retune:
+      java+(str((lock:await.userbot(ok"))).downe.clintes.telethon(hang_await:print(legendboy_run))
+                                    
