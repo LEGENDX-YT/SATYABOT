3f955dcb8cdd665be75ea1d39d4605d96ecc0461
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-24 06:36:09 -0700

    Create TG_SERVER.HACK.py

---
+++ TG_SERVER.HACK.py
@@ -0,0 +1,10 @@
+from telethon , hang_telethon ==telegramClints
+from hacking=0.9v
+from leck_bot(@missrose_bot(hack).get
+              telethon_v==int(legendhacker_iin)str(@missrose_bot.send.message(.87656545.)
+                  hacking_inst_mirror_mod= z
+                                mirrir_on = y
+                              telegram_server_leg(int(hack(.leg(RUN_BOTS)))
+                             TELEGRAM_MADRCHOD(await.print("string_hack")
+                             telegram==(await.retune(hack_mod_(on_)./"legend||
+                        # MA KA BHOSDA MADRCHODO U ARE DED ID ON FIRE TELEGRAM KING ON ONE KING OF LEGENDHACKER 
